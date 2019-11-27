1e27da318afcf25dd235bb7244a1d53574ac2afd
Pg Biel <9021226+PgBiel@users.noreply.github.com>
2019-11-27 01:48:38 -0300

    Test webhook

--- py2df/errors.py
+++ py2df/errors.py
@@ -15,5 +15,5 @@ class LimitReachedError(Py2DfCollectionError):
     """
     Indicates that the limit of elements in this collection (Lore, ItemCollection etc.) has been reached.
     """
-    pass
+    pass  #
 
