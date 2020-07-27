7445f7e5f0dc4bdb168f27de3d07570d260a26e2
Skezza <36114463+SkezzaB@users.noreply.github.com>
2020-07-27 14:25:59 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup()
+setuptools.setup(
+    name="Py2DF", # Replace with your own username
+    version="0.0.1",
+    author="PgBiel, Skezza",
+    author_email="author@example.com",
+    description="A tool to convert python scripts to DF code templates.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Py2DF/Py2DF",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
