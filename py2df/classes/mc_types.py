7918fdeaa3fdf1299a2945d08b5755a89158b554
jamster02 <63320517+jamster02@users.noreply.github.com>
2020-07-28 17:18:25 -0500

    Update py2df/classes/mc_types.py
    Co-authored-by: Pg Biel <9021226+PgBiel@users.noreply.github.com>

--- py2df/classes/mc_types.py
+++ py2df/classes/mc_types.py
@@ -278,7 +278,11 @@ class Item(DFType, Itemable):  # TODO: Bonus Item classes - WrittenBook, for exa
                 tag["EntityTag"] = ent_t
 
         if self.enchantments:
-            tag["Enchantments"] = [ItemEnchantmentSchema(dict(id='minecraft:' + enchant.ench_type.value, lvl=enchant.level)) for enchant in self.enchantments]
+            tag["Enchantments"] = [
+                ItemEnchantmentSchema(
+                    dict(id=f"minecraft:{enchant.ench_type.value}", lvl=enchant.level)
+                ) for enchant in self.enchantments
+            ]
         
         if any([self.leather_armor_color is not None, self.name, self.lore]):
             display = ItemDisplaySchema()
